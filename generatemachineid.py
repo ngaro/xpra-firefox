82e3bea6c192fadbe9c053e44d2926d7e5ccb909
Nikolas Garofil <nikolas@garofil.be>
2024-08-26 12:06:32 +0200

    Rewrite generatemachineid.py

--- generatemachineid.py
+++ generatemachineid.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import random
-machineid=""
-for i in range(0, 32): machineid += str(random.choice([0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']))
+machineid = ''.join(random.choices('0123456789abcdef', k=32))
 print(machineid)
